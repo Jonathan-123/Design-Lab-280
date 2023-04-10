1a5b1da0abd0f2a0c52f179de91073cd0bb2a77f
Jonathan-123 <36493826+Jonathan-123@users.noreply.github.com>
2023-04-10 04:09:53 -0500

    Added Remote Control Software - V2.1
    main changes include inverting the values so that the car drives in the proper direction

---
+++ code/ControlSystems/V2.0/leftJoystick.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+# contains and uses this version of Jason White's joystick.c at bottom
+# https://gist.github.com/jasonwhite/c5b2048c15993d285130/eb3bcd6e1cd88002d21764b9965d075eceaf4b83
+#
+# requires SNES joystick connected to Raspberry USB, pigpio library installed
+#
+#contains serial communication code, hardcoded to use ttyUSB0
+
+
+js=/tmp/leftjoystick
+if [ ! -f $js ]; then sed -n "/^\/\*\*$/,\$p" $0 | gcc -x c - -o $js; fi
+
+g=1500
+l=1500
+u=600
+p=1500
+d=100
+calib=0
+
+#port=/dev/ttyUSB0
+#echo "Setting up serial link to Arduino"
+#stty -F $port 115200 -hupcl cs8 cread min 0 time 10
+#stty -F $port 230400 -hupcl -echo cs8 cread min 0 time 10
+echo "Ready"
+
+while IFS= read -r line; do
+
+
+echo $line
+#echo $line > $port
+echo $line > leftjsdata.txt
+#cat joystickdata.txt 
+#sleep .1
+done < <($js)
+
+exit
+/**
+ * Author: Jason White
+ *
+ * Description:
+ * Reads joystick/gamepad events and displays them.
+ *
+ * Compile:
+ * gcc joystick.c -o joystick
+ *
+ * Run:
+ * ./joystick [/dev/input/jsX]
+ *
+ * See also:
+ * https://www.kernel.org/doc/Documentation/input/joystick-api.txt
+ */
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/joystick.h>
+
+int LEFT = 0;			//Store the value of each joystick, making 
+int RIGHT = 0;			//the data persistent across reads
+int FAST = 1;
+
+/**
+ * Reads a joystick event from the joystick device.
+ *
+ * Returns 0 on success. Otherwise -1 is returned.
+ */
+int read_event(int fd, struct js_event *event)
+{
+    ssize_t bytes;
+
+    bytes = read(fd, event, sizeof(*event));
+
+    if (bytes == sizeof(*event))
+        return 0;
+
+    /* Error, could not read full event. */
+    return -1;
+}
+
+/**
+ * Returns the number of axes on the controller or 0 if an error occurs.
+ */
+size_t get_axis_count(int fd)
+{
+    __u8 axes;
+
+    if (ioctl(fd, JSIOCGAXES, &axes) == -1)
+        return 0;
+
+    return axes;
+}
+
+/**
+ * Returns the number of buttons on the controller or 0 if an error occurs.
+ */
+size_t get_button_count(int fd)
+{
+    __u8 buttons;
+    if (ioctl(fd, JSIOCGBUTTONS, &buttons) == -1)
+        return 0;
+
+    return buttons;
+}
+
+/**
+ * Current state of an axis.
+ */
+struct axis_state {
+    short x, y;
+};
+
+/**
+ * Keeps track of the current axis state.
+ *
+ * NOTE: This function assumes that axes are numbered starting from 0, and that
+ * the X axis is an even number, and the Y axis is an odd number. However, this
+ * is usually a safe assumption.
+ *
+ * Returns the axis that the event indicated.
+ */
+size_t get_axis_state(struct js_event *event, struct axis_state axes[3])
+{
+    size_t axis = event->number / 2;
+
+    if (axis < 3)
+    {
+        if (event->number % 2 == 0)
+            axes[axis].x = event->value;
+        else
+            axes[axis].y = event->value;
+    }
+
+    return axis;
+}
+
+int main(int argc, char *argv[])
+{
+    const char *device;
+    int js;
+    struct js_event event;
+    struct axis_state axes[3] = {0};
+    size_t axis;
+
+    if (argc > 1)
+        device = argv[1];
+    else
+        device = "/dev/input/js0";
+
+    js = open(device, O_RDONLY);
+
+    if (js == -1)
+        perror("Could not open joystick");
+
+    /* This loop will exit if the controller is unplugged. */
+    while (read_event(js, &event) == 0)
+    {
+        switch (event.type)
+        {
+            case JS_EVENT_BUTTON:
+                printf("Button %u %s\n", event.number, event.value ? "pressed" : "released");
+				if (event.number = 5){
+					if (event.value == 1) FAST = 4;
+					else FAST = 1;
+				}
+                break;
+            case JS_EVENT_AXIS:
+                axis = get_axis_state(&event, axes);
+                if (axis < 3)
+                   if (axis < 1)
+						LEFT = (axes[axis].y) * FAST * -1;
+                   if (axis > 1)
+						RIGHT = (axes[axis].x) * FAST * -1;
+                   printf("%6dL\t%6dR\n", LEFT, RIGHT);
+					
+                break;
+            default:
+                /* Ignore init events. */
+                break;
+        }
+        fflush(stdout);
+    }
+
+    close(js);
+    return 0;
+}
---
+++ code/ControlSystems/V2.0/remotejs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+port=/dev/ttyUSB0
+baudrate=9600
+x=1
+
+echo "Setting up Serial link at a baudrate of $baudrate"
+stty -F $port $baudrate -hupcl -echo cs8 cread min 0 time 10
+echo "reading serial line"
+
+while [ $x = 1 ]; do
+#cat joystickdata.txt > $port
+#cat rightjsdata.txt > $port
+#cat rightjsdata.txt 
+#sleep .01
+cat leftjsdata.txt > $port
+cat leftjsdata.txt 
+sleep .01
+#cat joystickdata.txt
+#sleep 0.1
+done
